442f238a95631ea4daed41a9027fa123023a49aa
Jessica Yeh <jessicayeh@gmail.com>
2021-12-31 18:16:45 -0600

    different 6key version

--- 6key_multi_key_set.py
+++ 6key_multi_key_set.py
@@ -6,38 +6,46 @@ import adafruit_dotstar
 import usb_hid
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.keyboard import Keyboard
-from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 
-# Keycode options: https://circuitpython.readthedocs.io/projects/hid/en/latest/api.html#adafruit-hid-keycode-keycode
-# ConsumerControlCode options: https://circuitpython.readthedocs.io/projects/hid/en/latest/api.html#adafruit-hid-consumer-control-code-consumercontrolcode
-KEY_CODES = [
-    # top left
-    ConsumerControlCode.MUTE,
-    # top middle
-    [Keycode.CONTROL, Keycode.C],
-    # top right
-    [Keycode.CONTROL, Keycode.V],
-    # bottom left
-    Keycode.LEFT_ARROW,
-    # bottom middle
-    Keycode.RIGHT_ARROW,
-    # bottom right
-    "hello world",
-]
-
-time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
-cc = ConsumerControl(usb_hid.devices)
-kbd = Keyboard(usb_hid.devices)
-kbd_layout = KeyboardLayoutUS(kbd)
 cols = [digitalio.DigitalInOut(x) for x in (board.D11, board.D12, board.D13)]
 rows = [digitalio.DigitalInOut(x) for x in (board.D9, board.D10)]
 keys = (
     (0,1,2),
     (3,4,5),
 )
+
+def get_key_set(num):
+    if (num == 1):
+        return [
+            Keycode.LEFT_BRACKET,
+            Keycode.UP_ARROW,
+            Keycode.RIGHT_BRACKET,
+            Keycode.LEFT_ARROW,
+            Keycode.DOWN_ARROW,
+            Keycode.RIGHT_ARROW,
+        ]
+    elif (num == 2):
+        return [
+            ConsumerControlCode.MUTE,
+            ConsumerControlCode.PLAY_PAUSE,
+            [Keycode.WINDOWS, Keycode.LEFT_BRACKET, Keycode.RIGHT_BRACKET],
+            [Keycode.WINDOWS, Keycode.LEFT_BRACKET, Keycode.RIGHT_BRACKET],
+            ConsumerControlCode.VOLUME_DECREMENT,
+            ConsumerControlCode.VOLUME_INCREMENT,
+        ]
+    else:
+        return []
+
+current_key_set = 1
+KEYS = get_key_set(current_key_set)
+
+time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
+cc = ConsumerControl(usb_hid.devices)
+kbd = Keyboard(usb_hid.devices)
 keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
+led = adafruit_dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.1)
 
 # This dict is just for being able to tell if a keycode is a cc code or normal code,
 # to be able to send to the proper device (cc or kbd)
@@ -62,25 +70,35 @@ last_keys = []
 while True:
     keys = keypad.pressed_keys
 
+    switch_key_set = list(set(keys) ^ set([3,4,5])) == []
+    if switch_key_set:
+        led[0] = (25, 25, 25)
+        if current_key_set == 1:
+            current_key_set = 2
+        else:
+            current_key_set = 1
+        KEYS = get_key_set(current_key_set)
+        cc.release()
+        kbd.release_all()
+        time.sleep(1)
+        led[0] = (0, 0, 0)
+        continue
+
     removed_keys = list(set(last_keys) - set(keys))
     for key in removed_keys:
-        keycode = KEY_CODES[key]
-        is_str = isinstance(keycode, str)
+        keycode = KEYS[key]
         is_list = isinstance(keycode, list)
         if not is_list and CONSUMER_CONTROL_CODES.get(keycode):
             cc.release()
-        elif not is_str:
+        else:
             kbd.release(*keycode) if is_list else kbd.release(keycode)
 
     added_keys = list(set(keys) - set(last_keys))
     for key in added_keys:
-        keycode = KEY_CODES[key]
-        is_str = isinstance(keycode, str)
+        keycode = KEYS[key]
         is_list = isinstance(keycode, list)
         if not is_list and CONSUMER_CONTROL_CODES.get(keycode):
             cc.press(keycode)
-        elif is_str:
-            kbd_layout.write(keycode)
         else:
             kbd.press(*keycode) if is_list else kbd.press(keycode)
 
